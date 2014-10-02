95a0799ebdf323c12d531ce16217f09e23b7fc60
Andrea Anaya <andrayantelo@gmail.com>
2014-10-02 15:19:05 -0500

    updated keywords

--- keywords.py
+++ keywords.py
@@ -24,29 +24,5 @@ BOAT = 'boat'
 DRINK = 'drink'
 
 
-def parse_commands(command):
-    command_dictionary = {'sniff': self.sniff_text,
-                          'look': self.look_text,
-                          'nothing': self.nothing_text,
-                          'play': self.play_text,
-                          'dig': self.dig_text,
-                          'bark': self.bark_text,
-                          'roll': self.roll_text,
-                          'walk': self.walk_text,
-                          'run': self.run_text,
-                          'stand': self.stand_text,
-                          'find': self.find_text,
-                          'inside': self.inside_text,
-                          'back': self.back_text,
-                          'forward': self.forward_text,
-                          'items': self.display_items_text,
-                          'enter': self.enter_tunneltext,
-                          'trail': self.trail_text,
-                          'sword': self.use_sword,
-                          'swim': self.swim_text,
-                          'boat': self.use_boat,
-                          'drink': self.drink_water
-                          }
-                          
-    print command_dictionary.get(command)
+
         
